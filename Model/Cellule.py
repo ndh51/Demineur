09a31f1bcbdb51b1c84ae0b8e506d81aa589c363
hale0010 <nadhir.hales@etudiant.univ-reims.fr>
2023-01-10 22:54:31 +0100

    construireCellule : tests ok

--- Model/Cellule.py
+++ Model/Cellule.py
@@ -3,10 +3,11 @@
 
 from Model.Constantes import *
 
+
 #
 # Modélisation d'une cellule de la grille d'un démineur
 # 
- 
+
 
 def type_cellule(cell: dict) -> bool:
     """
@@ -19,10 +20,26 @@ def type_cellule(cell: dict) -> bool:
         and type(cell[const.VISIBLE] == bool) and type(cell[const.CONTENU]) == int \
         and (0 <= cell[const.CONTENU] <= 8 or cell[const.CONTENU] == const.ID_MINE)
 
-def isContenuCorrect(ent:int)->bool:
-    if type(ent)==int and (0 <= ent <= 8 or ent == const.ID_MINE):
+
+def isContenuCorrect(ent: int) -> bool:
+    if type(ent) == int and (0 <= ent <= 8 or ent == const.ID_MINE):
         res = True
-    else :
+    else:
         res = False
+    return res
+
+
+def construireCellule(ent: int, VF : bool) -> dict:
+    if not isContenuCorrect(ent) or ent is None or VF is None:
+        raise ValueError("construireCellule : le contenu valeur_du_contenu n’est pas correct")
+    elif type(VF)!= bool :
+        raise TypeError("construireCellule : le second paramètre (type_du_paramètre) n’est pas un booléen")
+    else:
+        ent = 0
+        VF = False
+        res = {}
+        res[const.CONTENU] = ent
+        res[const.VISIBLE] = VF
+
     return res
 
